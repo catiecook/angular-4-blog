3ae48483cb04a4db6e66d5abadf87e7f8ad40422
catiecook <catiecook888@gmail.com>
2018-01-25 09:43:05 -0800

    got pages working

---
+++ src/app/app-grid/app-grid.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../material/material.module'
+
+@Component({
+  selector: 'app-grid',
+  templateUrl: './app-grid.component.html',
+  styleUrls: ['./app-grid.component.scss']
+})
+export class AppGridComponent { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Blog of Sorts';
-  description = "Blog to talk about whatever";
 }
