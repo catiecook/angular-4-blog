a5fc27220273563f90546d2b38c2c8ec1d139274
catiecook <catiecook888@gmail.com>
2018-02-01 08:31:45 -0800

    removed app-grip because it was too clunky - switched to normal css and html

--- src/app/app-grid/app-grid.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { MaterialModule } from '../material/material.module'
-
-@Component({
-  selector: 'app-grid',
-  templateUrl: './app-grid.component.html',
-  styleUrls: ['./app-grid.component.scss']
-})
-export class AppGridComponent { }
